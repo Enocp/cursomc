7e6fdc37fadfd444a9f39b27350ec1d47a15d5f7
ENOC PIERRE <pierre.enoc@gmail.com>
2019-12-31 20:47:16 -0200

    Endpoint/pedidos/{id} disponibilizado

--- src/main/java/com/pierre/cursomc/domain/ItemPedido.java
+++ src/main/java/com/pierre/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido  implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+  
+  @JsonIgnore
   @EmbeddedId
   private ItemPedidoPk id = new ItemPedidoPk();
 	
@@ -27,11 +30,15 @@ public ItemPedido(Pedido pedido, Produto protudo, Double desconto, Integer quant
 	this.quantidade = quantidade;
 	this.preco = preco;
 }
-
+  @JsonIgnore
   public Pedido getpedido() {
 	  return id.getPedido();
   }
   
+
+  public Produto getProduto() {
+	  return id.getProduto();
+  }
   
 
 public ItemPedidoPk getId() {
