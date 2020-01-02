2cf127a02bcb228822f058b88e80025a338a3981
ENOC PIERRE <pierre.enoc@gmail.com>
2020-01-01 23:36:05 -0200

    listando todas categoria com DTO

--- src/main/java/com/pierre/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/pierre/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,14 +1,11 @@
 package com.pierre.cursomc.resources.exception;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.HttpStatusCodeException;
 
 import com.pierre.cursomc.services.exceptions.DataIntegrityException;
 import com.pierre.cursomc.services.exceptions.ObjectNotfoundException;
@@ -23,7 +20,7 @@ public class ResourceExceptionHandler{
 	}
     
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandreError> dataIntegrity(ObjectNotfoundException e, HttpServletRequest request){
+	public ResponseEntity<StandreError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		StandreError err = new StandreError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
