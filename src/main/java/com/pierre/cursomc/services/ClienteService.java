52ae514c53a75864968818db5eaf55867974e4f4
ENOC PIERRE <pierre.enoc@gmail.com>
2020-01-05 15:16:07 -0300

    put,delete,get para cliente

--- src/main/java/com/pierre/cursomc/services/ClienteService.java
+++ src/main/java/com/pierre/cursomc/services/ClienteService.java
@@ -1,14 +1,21 @@
 package com.pierre.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pierre.cursomc.domain.Cliente;
+import com.pierre.cursomc.domain.Cliente;
+import com.pierre.cursomc.dto.ClienteDTO;
 import com.pierre.cursomc.repositories.ClienteRepository;
-
+import com.pierre.cursomc.services.exceptions.DataIntegrityException;
 import com.pierre.cursomc.services.exceptions.ObjectNotfoundException;
 @Service
 public class ClienteService {
@@ -21,5 +28,39 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotfoundException(
 			"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
-
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+	}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir porque há entidades relacionais");	
+		}
+	}
+	
+	public List<Cliente>findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		//return new Cliente(objDTO.getId(), objDTO.getNome());
+		//throw new UnsupportedOperationException();
+		
+		return new Cliente(objDTO.getId(),objDTO.getNome(),objDTO.getEmail(),null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
