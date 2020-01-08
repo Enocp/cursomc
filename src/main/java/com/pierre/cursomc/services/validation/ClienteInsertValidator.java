58222eaaccc08bf223f9c85cbcf2585d0599b9ac
ENOC PIERRE <pierre.enoc@gmail.com>
2020-01-07 22:47:35 -0300

    validacao customizada email nao repedido na insercao de cliente

--- src/main/java/com/pierre/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/pierre/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pierre.cursomc.domain.Cliente;
 import com.pierre.cursomc.domain.enums.TipoCliente;
 import com.pierre.cursomc.dto.ClienteNewDTO;
+import com.pierre.cursomc.repositories.ClienteRepository;
 import com.pierre.cursomc.resources.exception.FieldMessage;
 import com.pierre.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ invalido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		 if(aux != null) {
+			 list.add(new FieldMessage("email", "Email já existe"));
+		 }
        // inclua os testes aqui, inserindo erros na lista
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
