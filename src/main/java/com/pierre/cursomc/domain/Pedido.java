5a44eba65e575df17268e26d12ac7a0f3566ea32
ENOC PIERRE <pierre.enoc@gmail.com>
2020-01-21 21:20:54 -0300

    Inserindo pedido

--- src/main/java/com/pierre/cursomc/domain/Pedido.java
+++ src/main/java/com/pierre/cursomc/domain/Pedido.java
@@ -30,7 +30,7 @@ public class Pedido implements Serializable{
   @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
   private Date instante;
   
-  @JsonManagedReference
+  //@JsonManagedReference
   @OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
   private Pagamento pagamento;
   
--- src/main/java/com/pierre/cursomc/resources/PedidoResource.java
+++ src/main/java/com/pierre/cursomc/resources/PedidoResource.java
@@ -1,13 +1,20 @@
 package com.pierre.cursomc.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pierre.cursomc.domain.Pedido;
+import com.pierre.cursomc.dto.CategoriaDTO;
 import com.pierre.cursomc.services.PedidoService;
 
 @RestController
@@ -24,5 +31,14 @@ public class PedidoResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	 
+		@RequestMapping(method = RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody Pedido obj){
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+		}
 
 }
