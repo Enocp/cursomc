3c89edf7c3ea2cba4f520815f996297b185c0ac7
ENOC PIERRE <pierre.enoc@gmail.com>
2020-01-27 22:14:16 -0300

    implementando smtp Email service com servidor do google

--- src/main/java/com/pierre/cursomc/config/DevConfig.java
+++ src/main/java/com/pierre/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.pierre.cursomc.services.DBService;
+import com.pierre.cursomc.services.EmailService;
+import com.pierre.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -27,6 +29,11 @@ public class DevConfig {
 		
 		dbService.instatiateTestDataBase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
